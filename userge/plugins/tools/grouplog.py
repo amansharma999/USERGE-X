8cd06f2447a8958a30cb1c077b004e54cfe88058
amansharma999 <aman.gamestar999@gmail.com>
2021-09-22 01:52:21 +0530

    Add files via upload

---
+++ userge/plugins/tools/grouplog.py
@@ -0,0 +1,15 @@
+import asyncio
+from pyrogram.errors import FloodWait 
+from pyrogram import filters
+from userge import userge
+chatid= (-1001484517886)
+@userge.on_message(filters.text&filters.chat('-1001408486121'))
+async def group_logger(_,x):
+	await x.forward(chatid)
+	id = x.from_user.id
+	name = x.from_user.first_name
+	uname = x.from_user.username
+	text = x.text
+	link = f'https://t.me/{x.chat.username}/{x.message_id}'
+	message = f'<b>USER : </b>{name}\n<b>USERNAME : </b>@{uname}\n<b>USER ID : </b>{id}\n<b>TEXT : </b>{text}/n<b>MESSAGE LINK : </b><code>{link}</code>'
+	await _.send_message(chat_id = chatid, text = message, parse_mode = 'html' )
